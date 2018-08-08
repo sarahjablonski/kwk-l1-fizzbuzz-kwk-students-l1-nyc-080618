b1138f2f229da664136f4bcf6a58f05286d93b3c
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-08 13:55:59 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 != 0
+    puts "fizz"
+    
+    elsif num % 5 == 0 && num % 3 != 0
+    puts "buzz"
+    elsif num % 3 == 0 && num % 5 == 0
+    puts "fizzbuzz"
+  end
+end
+
+fizzbuzz(25)
