0aded89207adcbc40ad747e8350de268902f6b88
sarahjablonski <shapeshiftwithme@gmail.com>
2018-08-08 14:00:28 +0000

    automatically backed up by learn

---
+++ dancing.rb
@@ -0,0 +1,9 @@
+#write a while loop 
+#output "step to the left"/"step to the right" 5 times 
+
+counter = 0
+while counter < 5
+puts "step to the left"
+puts "step to the right"
+counter = counter + 1
+end
